2cc0b010ba036d4329d9feb8071abcda4a3e5a80
deekshaps <42870475+deekshaps@users.noreply.github.com>
2018-10-05 11:55:49 +0530

    Create day 2.c

---
+++ day 2.c
@@ -0,0 +1,58 @@
+1.Write a program to get a number from the user and print it?
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a number:\n");
+    scanf("%d",&n);
+    printf("number entered by the user is:%d",n);
+
+    return 0;
+}
+
+
+
+2.What is #include <stdio.h>?
+This file contains so-called function prototypes of functions such as printf() , scanf() , ... so that compiler knows what are their parameters and return values.
+
+
+
+3.What is printf?
+printf function is used to print the character, string, float, integer values onto the output screen.
+
+
+4.What is main?
+main is the entry point of the program.
+
+
+5.What is a variable?
+Variable is which we can change i.e., has no fixed values.
+
+
+6.What is type of a variable?
+local variables and global variables
+
+
+7.What & operator?
+& operator is used to indicate the address of a variable.
+
+
+8.What is %d in printf and scanf?
+%d refers to the integer. It is a format specifier.
+
+
+9.What is the meaning of compilation of a program?
+Compilation of program means converting high level language into machine level language.
+
+
+10.What is the meaning of running a program?
+Running the program means executing the code as per the code is written.
+
+
+11.List all syntax errors and reasons for the syntax errors.
+Syntax error means not following the rule of declaring the function or missing important symbols.
+
+
+12. Missing paranthesis, Missing commas,semicolons, writing wrong printf or scanf function.
+
